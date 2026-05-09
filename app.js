a94ce6fd7ea28beea60c56b230eef9c0b3ff2985
jeronimo1970rojas-lab <jeronimo1970rojas@gmail.com>
2026-05-09 14:28:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -255,7 +255,7 @@ function mostrarPanel(data) {
               src="logo.png"
               class="topbar-logo">
 <img
-  src="https://jeronimo1970rojas-lab.github.io/${data.usuarios}.jpg"
+  src="https://jeronimo1970rojas-lab.github.io/${data.usuario}.jpg"
   style="
     width:55px;
     height:55px;
